2ce55a4e8d9a76b4761edf1f5308f5a73d55608c
Utkarsh Jaiswal <utkarshjaiswal2580@gmail.com>
2023-06-02 00:25:24 +0530

    Initial Commit

---
+++ src/main/java/com/leetcodeapi/LeetcodeApiApplication.java
@@ -0,0 +1,13 @@
+package com.leetcodeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetcodeApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leetcodeapi/LeetcodeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leetcodeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
